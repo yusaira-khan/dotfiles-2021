6a5be015797ce8742fc3a9b996aae72594e5ac61
Yusaira Khan <yusairak@nulogy.com>
2026-02-18 11:05:45 -0500

    Update

--- zsh/.grml_utilities/primary.zsh
+++ zsh/.grml_utilities/primary.zsh
@@ -3,15 +3,44 @@ ls_options+=( -v )
 export CLICOLOR=1
 REPORTTIME=5
 
-
-
+# append history list to the history file; this is the default but we make sure
+# because it's required for share_history.
 setopt append_history
-setopt auto_cd
+
+# import new commands from the history file also in other zsh-session
+setopt share_history
+
+# save each command's beginning timestamp and the duration to the history file
+setopt extended_history
+
+# If a new command line being added to the history list duplicates an older
+# one, the older command is removed from the list
+setopt histignorealldups
+
+# NOT USING: if a command is issued that can't be executed as a normal command, and the
+# command is the name of a directory, perform the cd command to that directory.
+#setopt auto_cd
+
+# make cd push the old directory onto the directory stack.
 setopt auto_pushd
+
+# don't push the same dir twice.
 setopt pushd_ignore_dups
+
+# use zsh style word splitting
 setopt noshwordsplit
+
+# allow comments in interactive terminals
 setopt interactivecomments
+
 setopt print_exit_value
+
+# whenever a command completion is attempted, make sure the entire command path
+# is hashed first.
+setopt hash_list_all
+
+# not just at the end
+setopt completeinword
 
 
 # grep for running process, like: 'any vim'
@@ -33,29 +62,41 @@ function cl () {
 }
 
 
-#List files with colors ls [...])
-alias ls="command lsd ${ls_options:+${ls_options[*]}}"
-# List all files, with colors ls [...])
-alias la="command lsd -la ${ls_options:+${ls_options[*]}}"
-#List files with long colored list, without dotfiles ls -l [...])
-alias ll="command lsd -l ${ls_options:+${ls_options[*]}}"
-# List files with long colored list, human readable sizes ls -hAl [...])
-alias lh="command lsd -hAl ${ls_options:+${ls_options[*]}}"
-# List files with long colored list, append qualifier to filenames ls -l [...]. / for directories, @ for symlinks
-alias l="command lsd -l ${ls_options:+${ls_options[*]}}"
+# listing stuff
+# Execute \kbd{ls -lSrah}
+alias dir="command ls -lSrah"
 # Only show dot-directories
-alias lsdotdir='command lsd -d .*(/)'
+alias lad='command ls -d .*(/)'
 # Only show dot-files
-alias lsdotfiles='command lsd -a .*(.)'
+alias lsa='command ls -a .*(.)'
+# Only files with setgid/setuid/sticky flag
+alias lss='command ls -l *(s,S,t)'
+# Only show symlinks
+alias lsl='command ls -l *(@)'
+# Display only executables
+alias lsx='command ls -l *(*)'
+# Display world-{readable,writable,executable} files
+alias lsw='command ls -ld *(R,W,X.^ND/)'
 # Display the ten biggest files
-alias lsbig="command lsd -flh *(.OL[1,10])"
-alias lsnice="command lsd -lSrah"
+alias lsbig="command ls -flh *(.OL[1,10])"
+# Only show directories
+alias lsd='command ls -d *(/)'
+# Only show empty directories
+alias lse='command ls -d *(/^F)'
 # Display the ten newest files
-alias lsnew="command lsd -rtlh *(D.om[1,10])"
+alias lsnew="command ls -rtlh *(D.om[1,10])"
 # Display the ten oldest files
-alias lsold="command lsd -rtlh *(D.Om[1,10])"
+alias lsold="command ls -rtlh *(D.Om[1,10])"
 # Display the ten smallest files
-alias lssmall="command lsd -Srl *(.oL[1,10])"
+alias lssmall="command ls -Srl *(.oL[1,10])"
+# Display the ten newest directories and ten newest .directories
+alias lsnewdir="command ls -rthdl *(/om[1,10]) .*(D/om[1,10])"
+# Display the ten oldest directories and ten oldest .directories
+alias lsolddir="command ls -rthdl *(/Om[1,10]) .*(D/Om[1,10])"
+
+# Remove current empty directory. Execute \kbd{cd ..; rmdir \$OLDCWD}
+alias rmcdir='cd ..; rmdir $OLDPWD || cd $OLDPWD'
+alias ...='cd ../../'
 
 # Create Directory and cd to it
 function mkcd () {
